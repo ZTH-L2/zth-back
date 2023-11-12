6755219ebf17f30ae9019570c92bbe960eaf6d22
Aekhy <102717242+Aekhy@users.noreply.github.com>
2023-11-12 23:28:33 +0100

    remove some bad design

--- app/index.php
+++ app/index.php
@@ -1,16 +1,5 @@
 <?php
-session_start();
 $requestUri = $_SERVER['REQUEST_URI'];
-
-// this little code is not suppose to represent 
-// how we will do it
-// Check if the request is for api_dummy
-if ($requestUri === "/api/dummy") {
-    // Redirect to api_dummy.php
-    $_SESSION["path"] = True;
-    header("Location: /api/api_dummy.php");
-    exit;
-}
 
 echo "Success\n";
 echo "your uri : " . $requestUri;
