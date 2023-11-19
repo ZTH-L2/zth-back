5c924370735a77bb1ff6c95688e8ff6186cbd19f
ElianGARRIGUES <elian.garrigues@hotmail.fr>
2023-11-19 19:19:34 +0100

    fix majors api

--- app/api/majors/controlleur.php
+++ app/api/majors/controlleur.php
@@ -11,11 +11,17 @@ function option_major($params){
 }
 
 function get_major($params){
-
-            $conn = db_connect();
-            $id = $params[0];
-            return json_encode(["succes"=>true,"message"=>select_major($conn, $id)]);
-
+    $conn = db_connect();
+    $id = $params[0];
+    $res = select_major($conn, $id);
+    if (is_null($res))
+    {
+        return json_encode([]);
+    }
+    else
+    {
+        return json_encode($res);
+    }
 }
 
 function post_major($params){
@@ -46,8 +52,15 @@ function post_major($params){
                     unsafe_data_error_message();
                     return;
                 }
-
-                return json_encode(["succes" => true, "message" => create_major($conn, $name)]);
+                $res = create_major($conn, $name);
+                if ($res)
+                {
+                    return success_message_json(201, "201 Created: New major successfully created");
+                }
+                else
+                {
+                    $res = error_message_json(500, "500 Internal Server Error: Could not create the major");
+                }
             }
             else
             {
@@ -70,17 +83,26 @@ function del_major($params){
     {
         if (is_admin())
         {
-    return json_encode(["succes"=>true,"message"=>delete_major(db_connect(), $params[0])]);
-}
-else
-{
-    return permission_denied_error_message();
-}
-}
-else
-{
-return authentification_required_error_message();
-}
+            $conn = db_connect();
+            delete_major($conn, $params[0]);
+            if (mysqli_affected_rows($conn) > 0)
+            {
+                return success_message_json(200, "200 OK: Deleted major successfully");
+            }
+            else
+            {
+                return success_message_json(200, "200 OK: Deleted nothing but successfull");
+            }
+        }
+        else
+        {
+            return permission_denied_error_message();
+        }
+    }
+    else
+    {
+        return authentification_required_error_message();
+    }
 }
 
 function put_major($params){
@@ -88,47 +110,54 @@ function put_major($params){
     {
         if (is_admin())
         {
-    if (update_post_var())
-    {
-    $conn = db_connect();
+            if (update_post_var())
+            {
+                $conn = db_connect();
+            
+                // get the data
+                if (isset($_POST["name"]) && (isset($_POST["id_major"])))
+                {
+                    $name_dirty = $_POST["name"];
+                    $id_dirty = $_POST["id_major"];
+                }
+                else
+                {
+                    invalid_format_data_error_message();
+                    return;
+                }
+            
+                // sanitize the data
+                $name = filter_var($name_dirty);
+                $id = filter_var($id_dirty, "FILTER_VALIDATE_INT");
 
-    // get the data
-    if (isset($_POST["name"]) && (isset($_POST["id_major"])))
-    {
-        $name_dirty = $_POST["name"];
-        $id_dirty = $_POST["id_major"];
+                if (!$name)
+                {
+                    unsafe_data_error_message();
+                    return;
+                }
+            
+                $res = json_encode(["succes" => true, "message" => update_major($conn, $name, $id)]);
+                if ($res)
+                {
+                    return success_message_json(200, "200 OK: Updated major's information successfully.") ;
+                }
+                else
+                {
+                    return error_message_json(500, "500 Internal Server Error: Could not update major's information.");
+                }
+            }
+            else
+            {
+                no_data_error_message();   
+            }
+        }
+        else
+        {
+            return permission_denied_error_message();
+        }
     }
     else
     {
-        invalid_format_data_error_message();
-        return;
+        return authentification_required_error_message();
     }
-
-    // sanitize the data
-    $name = filter_var($name_dirty);
-    $id = filter_var($id_dirty);
-
-
-    if (!$name)
-    {
-        unsafe_data_error_message();
-        return;
-    }
-
-    return json_encode(["succes" => true, "message" => update_major($conn, $name, $id)]);
-}
-
-else{
-    no_data_error_message();   
-}
-}
-else
-{
-    return permission_denied_error_message();
-}
-}
-else
-{
-return authentification_required_error_message();
-}
 }
