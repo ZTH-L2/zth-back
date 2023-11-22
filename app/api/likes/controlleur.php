d3f0e655f76965b8a0d383aab4d830f5d8ab9f9d
Aekhy <102717242+Aekhy@users.noreply.github.com>
2023-11-22 13:52:14 +0100

    wip : likes

--- app/api/likes/controlleur.php
+++ app/api/likes/controlleur.php
@@ -13,9 +13,22 @@ function option_like($params){
 
 // TO DO
 function those_i_liked($params){
-    return; 
+    if (!is_logged_in())
+    {
+        return authentification_required_error_message();
+    }
+    $conn = db_connect();
+    
+    $page = $params[1];
+    $amount_per_page = $params[2];
+    $offset = ($page - 1) * $amount_per_page;
+
+    $conn = db_connect();
+    $likes = select_all_user_page_amount($conn, $_SESSION["id_user"], $amount_per_page, $offset);
+    
+    return json_encode($likes);
 }
-// TO DO
+
 function did_i_like_this($params){
     if (!is_logged_in())
     {
@@ -42,9 +55,51 @@ function did_i_like_this($params){
 
 }
 
-// TO DO
 function like_one($params){
-    return; 
+    if (!is_logged_in())
+    {
+        return authentification_required_error_message();
+    }
+    if(!update_post_var())
+    {
+        return no_data_error_message();
+    }
+    if(!isset($_POST["id_comment"]))
+    {
+        return invalid_format_data_error_message();
+    }
+    
+    $id_comment = filter_var($_POST["id_comment"], FILTER_VALIDATE_INT);
+    if (!$id_comment)
+    {
+        return invalid_data_error_message();
+    }
+
+    // check if comment exists
+    $conn = db_connect();
+    $comment = select_comment($conn, $id_comment);
+    if (count($comment) == 0)
+    {
+        return invalid_data_error_message();
+    }
+    // check if not already liked
+    $like = select_like_with_comment_and_user($conn, $_POST["id_comment"], $id_comment);
+    if (count($like) == 0)
+    {
+        return error_message_json(403, "403 Forbidden: You already liked this comment.");
+    }
+    // like the comment
+    $res = create_like($conn, $_SESSION["id_user"], $id_comment);
+    if($res)
+    {
+        http_response_code(201);
+        return;
+    }
+    else
+    {
+        return error_message_json(500, "500 Internal Server Error: Could not like the comment");
+    }
+
 }
 // TO DO
 function like_many($params){
@@ -61,5 +116,47 @@ function did_i_liked_those($params){
 
 // TO DO
 function unlike_one($params){
-    return; 
+    if (!is_logged_in())
+    {
+        return authentification_required_error_message();
+    }
+    if(!update_post_var())
+    {
+        return no_data_error_message();
+    }  
+    if(!isset($_POST["id_comment"]))
+    {
+        return invalid_format_data_error_message();
+    }
+    
+    $id_comment = filter_var($_POST["id_comment"], FILTER_VALIDATE_INT);
+    if (!$id_comment)
+    {
+        return invalid_data_error_message();
+    }
+
+    // check if comment exists
+    $conn = db_connect();
+    $comment = select_comment($conn, $id_comment);
+    if (count($comment) == 0)
+    {
+        return invalid_data_error_message();
+    }
+    // check if like exist
+    $like = select_like_with_comment_and_user($conn, $_POST["id_comment"], $id_comment);
+    if (count($like) != 0)
+    {
+        return error_message_json(403, "403 Forbidden: You already liked this comment.");
+    }
+    // unlike the comment
+    $res = delete_like($conn, $like["id_like"]);
+    if($res)
+    {
+        http_response_code(201);
+        return;
+    }
+    else
+    {
+        return error_message_json(500, "500 Internal Server Error: Could not like the comment");
+    }
 }
