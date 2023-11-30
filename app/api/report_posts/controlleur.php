5cd68306546dfaf7f5f654dd7c4bb908cbea1ded
Aekhy <102717242+Aekhy@users.noreply.github.com>
2023-11-30 19:54:47 +0100

    fixing the commit of report post

--- app/api/report_posts/controlleur.php
+++ app/api/report_posts/controlleur.php
@@ -1,61 +1,11 @@
 <?php
 
-header('Access-Control-Allow-Origin : *');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Methods : GET');
+header('Access-Control-Allow-Origin: *');
 
-require_once "modules/module.php";
-require_once "controlleur.php";
+require_once "api/utils/utils.php";
+require_once "api/db_connect.php";
 
-$dict = [
-    "OPTION" => [
-        "function" => 'option'
-
-    ],
-
-    "POST" => [
-        "params" => ["/comment/"],
-        "function" => 'create_report_comment'
-    ],
-
-    "GET" => [
-        "routes" => [
-            [
-                "params" => ["/^comment$/","/[0-9][report,]/","/[0-9][update,]/"],
-                "function" => 'update_report_comment'
-            ],
-
-            [
-                "params" => ["/^comment$/","/[0-9][report,]/","/[0-9][update,]/","/[0-9][parametres?,]/"],
-                "function" => 'update_report_comment_with_parameter'
-            ],
-
-            [
-                "params" => ["/^comment$/","/[0-9][report,]/","/[0-9][select,]/"],
-                "function" => 'select_report_comment'
-            ],
-
-            [
-                "params" => ["/^comment$/","/[0-9][report,]/","/[0-9][select,]/","/[0-9][all,]/"],
-                "function" => 'select_all_report_comment'
-            ],
-
-            [
-                "params" => ["/^comment$/","/[0-9][report,]/","/[0-9][select,]/","/[0-9][all,]/"],
-                "function" => 'select_all_report_post'
-            ],
-
-            [
-                "params" => ["/^comment$/","/[0-9][report,]/","/[0-9][select,]/","/[0-9][all,]/","/[0-9][parametres?,]/"],
-                "function" => 'select_all_report_comment_with_parameter'
-            ],
-
-            [
-                "params" => ["/^comment$/","/[0-9][report,]/","/[0-9][delete,]/"],
-                "function" => 'delete_report_comment'
-            ]
-        ]
-    ]
-];
-
-$user_module = new Module($dict);
+function option_report_post($params){
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Allow-Methods: GET, POST, DELETE');
+}
--- app/api/report_posts/report_post_module.php
+++ app/api/report_posts/report_post_module.php
@@ -1,62 +1,61 @@
 <?php
-
-header('Access-Control-Allow-Origin : *');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Methods : GET');
-
 require_once "modules/module.php";
 require_once "controlleur.php";
 
 $dict = [
     "OPTION" => [
-        "function" => 'option'
+        "function" => 'option_report_post'
 
     ],
 
     "POST" => [
-        "params" => ["/post/"],
-        "function" => 'create_report_post'
+        "params" => ["/^post$/"],
+        "function" => ''
     ],
 
     "GET" => [
         "routes" => [
             [
-                "params" => ["/post/"],
-                //if (select_report_post($conn, $id) != null) {
-                    "function" => 'create_report_post'
-                //} else {
-                //    return invalid_format_data_error_message();
-                //}
+                "params" => ["/^post$/"],
+                # TO DO
+                "function" => '' # someting that uses create_report_post
+
             ],
 
             [
-                "params" => ["/^post$/","/[0-9][report,]/","/[0-9][update,]/"],
-                "function" => 'update_report_post'
+                "params" => ["/^post$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/"],
+                # TO DO
+                "function" => '' # someting that uses update_report_post
             ],
 
             [
-                "params" => ["/^post$/","/[0-9][report,]/","/[0-9][update,]/","/[0-9][parametres?,]/"],
-                "function" => 'update_report_post_with_parameter'
+                "params" => ["/^post$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/"],
+                # TO DO
+                "function" => '' # someting that uses update_report_post_with_parameter
             ],
 
             [
-                "params" => ["/^post$/","/[0-9][report,]/","/[0-9][select,]/"],
-                "function" => 'select_report_post'
+                "params" => ["/^post$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/"],
+                # TO DO
+                "function" => '' # someting that uses select_report_post
             ],
 
             [
-                "params" => ["/^post$/","/[0-9][report,]/","/[0-9][select,]/","/[0-9][all,]/"],
-                "function" => 'select_all_report_post'
+                "params" => ["/^post$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/"],
+                # TO DO
+                "function" => '' # someting that uses select_all_report_post
             ],
 
             [
-                "params" => ["/^post$/","/[0-9][report,]/","/[0-9][select,]/","/[0-9][all,]/","/[0-9][parametres?,]/"],
-                "function" => 'select_all_report_post_with_parameter'
+                "params" => ["/^post$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/"],
+                # TO DO
+                "function" => '' # someting that uses select_all_report_post_with_parameter 
             ],
 
             [
-                "params" => ["/^post$/","/[0-9][report,]/","/[0-9][delete,]/"],
-                "function" => 'delete_report_post'
+                "params" => ["/^post$/","/^[0-9]{1,}$/","/^[0-9]{1,}$/"],
+                # TO DO
+                "function" => '' # someting that uses delete_report_post 
             ]
         ]
     ]
